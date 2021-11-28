f57d7f8ccdd18b7d2254552af123dd55d0fcf3a3
cocoraingenieros.com <miguel.rubio@ingenieros.com>
2021-11-28 16:46:59 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+//const path = require('path')
+//require('dotenv').config()
+const app = express()
+const authRoutes = require('./routes/auth.routes')
+
+// configuraciones
+app.set('port', process.env.PORT || 3000)
+//mongoose.connect(process.env.DB_STRING)
+//.then(db => console.log('Connected to Mongo'))
+//.catch(err => console.log(err))
+//app.use('/documentation', express.static(path.join(__dirname, '../doc/')))
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors())
+//app.use(express.urlencoded({
+    //extended: false
+//}))
+
+//rutas
+app.use('/auth', authRoutes)
+
+//inicio del servidor
+app.listen(app.get('port'), ()=>{
+    console.log('Server Running')
+})
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router()
+const msg = require('../helpers/messages')
+const User = require('../models/user')
+const authService = require('../services/auth.service')
+
+router.post('/register', async (req, res)=>{
+    try {
+        const user = new User(req.body)
+        let token = await authService.register(user)
+        //res.status(200).json({"token": token})
+        res.status(token.code).json(token);
+    } catch (error) {
+        res.send(error) 
+    }
+})  
+
+//router.post('/login', async (req, res)=>{
+router.post('login', async (req, res)=>{
+    try {
+        const {email, password} = req.body
+        if(!email || !password){
+            res.status(400).json(msg.fieldsRequired)
+        }
+        //let token = await authService.login(req.body)
+        const token = await authService.login(req.body)
+        //res.status(token.code).json({"token":token})
+        res.status(token.code).json(token)
+    } catch (error) {
+        res.send(error)
+        //res.status(500).json({'error':error})
+    }
+})
+
+module.exports = router
