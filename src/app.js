216f75b108e99caa57ac9a7b99bc9c77cf103733
cocoraingenieros.com <miguel.rubio@ingenieros.com>
2021-12-02 18:04:38 -0500

    m

--- src/app.js
+++ src/app.js
@@ -17,9 +17,9 @@ mongoose.connect(process.env.DB_STRING)
 // middlewares
 app.use(morgan('dev'))
 app.use(cors())
-//app.use(express.urlencoded({
-    //extended: false
-//}))
+app.use(express.urlencoded({
+    extended: false
+}))
 
 //rutas
 app.use('/auth', authRoutes)
